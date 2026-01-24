424a6780eb5511463318618665d882c997af13fa
Karthik <karthi12kathir@gmail.com>
2026-01-24 23:15:10 +0800

    Completed Level 0: Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Nimbus User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Nimbus.java
@@ -0,0 +1,20 @@
+public class Nimbus {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        // System.out.println("Hello from\n" + logo);
+        printLine();
+        System.out.println("Hello! I'm Nimbus\nWhat can I do for you?");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println("_____________________________________________");
+    }
+}
