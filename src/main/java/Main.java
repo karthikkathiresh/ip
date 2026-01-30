b3370528db9670801ce832d6c7fbb52c6ddcdcd4
Karthik <karthi12kathir@gmail.com>
2026-01-30 12:52:18 +0800

    Add ability to store texts entered by user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,26 +6,55 @@ public class Main {
         String line;
         Scanner in = new Scanner(System.in);
 
-        printLine();
-        System.out.println("    Hello! I'm Nimbus");
-        System.out.println("    What can I do for you?");
-        printLine();
+        printWelcomeMessage();
+
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         line = in.nextLine();
 
-        while (!line.equals("bye")) {
-            printLine();
-            System.out.println("    " + line);
-            printLine();
+        while (true) {
+            if (line.equals("list")) {
+                printLine();
+                System.out.println("    Here are the tasks in your list: ");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i + 1) + ". " + tasks[i].toString());
+                }
+                printLine();
+            } else if (line.equals("bye") || line.equals("Bye")) {
+                printGoodbyeMessage();
+                return;
+            } else {
+                tasks[taskCount] = new Task(line);
+                printAddedMessage(line);
+                taskCount++;
+            }
+
             line = in.nextLine();
         }
-
-        printLine();
-        System.out.println("    Bye. Hope to see you again soon!");
-        printLine();
     }
 
     public static void printLine() {
         System.out.println("    _____________________________________________");
     }
+
+    public static void printWelcomeMessage() {
+        printLine();
+        System.out.println("    Hello! I'm Nimbus");
+        System.out.println("    What can I do for you?");
+        printLine();
+    }
+
+    public static void printGoodbyeMessage() {
+        printLine();
+        System.out.println("    Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printAddedMessage(String task) {
+        printLine();
+        System.out.println("    added: " + task);
+        printLine();
+    }
+
 }
