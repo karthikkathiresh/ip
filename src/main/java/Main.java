ba684af20b381dfe29751289116ced8aa6616dde
Karthik <karthi12kathir@gmail.com>
2026-01-30 22:28:58 +0800

    Add the ability to mark completed tasks as done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,25 +2,25 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+
+    private static Task[] tasks = new Task[100];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
 
         printWelcomeMessage();
 
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
-
         line = in.nextLine();
 
         while (true) {
             if (line.equals("list")) {
-                printLine();
-                System.out.println("    Here are the tasks in your list: ");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println("    " + (i + 1) + ". " + tasks[i].toString());
-                }
-                printLine();
+                printListOfTasks();
+            } else if (line.startsWith("mark")) {
+                markTaskAsDone(line);
+            } else if (line.startsWith("unmark")) {
+                markTaskAsUndone(line);
             } else if (line.equals("bye") || line.equals("Bye")) {
                 printGoodbyeMessage();
                 return;
@@ -51,10 +51,46 @@ public class Main {
         printLine();
     }
 
+    public static void printListOfTasks() {
+        printLine();
+        System.out.println("    Here are the tasks in your list: ");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("    " + (i + 1) + ". " + tasks[i].toString());
+        }
+        printLine();
+    }
+
+    public static void markTaskAsDone(String line) {
+        String integerPart = line.split(" ")[1];
+        int listIndex = Integer.parseInt(integerPart) - 1;
+        if (listIndex >= taskCount) {
+            System.out.println("    Invalid task. Try again!");
+        } else {
+            tasks[listIndex].setIsDone(true);
+            printLine();
+            System.out.println("    Nice! I've marked this task as done: ");
+            System.out.println("    " + tasks[listIndex].getStatusIcon() + " " + tasks[listIndex].description);
+            printLine();
+        }
+    }
+
+    public static void markTaskAsUndone(String line) {
+        String integerPart = line.split(" ")[1];
+        int listIndex = Integer.parseInt(integerPart) - 1;
+        if (listIndex >= taskCount) {
+            System.out.println("    Invalid task. Try again!");
+        } else {
+            tasks[listIndex].setIsDone(false);
+            printLine();
+            System.out.println("    OK, I've marked this task as not done yet: ");
+            System.out.println("    " + tasks[listIndex].getStatusIcon() + " " + tasks[listIndex].description);
+            printLine();
+        }
+    }
+
     public static void printAddedMessage(String task) {
         printLine();
         System.out.println("    added: " + task);
         printLine();
     }
-
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,21 @@
 public class Task {
-    private final String description;
+    protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
     }
 
     public String toString() {
-        return this.description;
+        return this.getStatusIcon() + " " + this.description;
+    }
+
+    public void setIsDone(boolean bool) {
+        this.isDone = bool;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone) ? "[X]" : "[]";
     }
 }
